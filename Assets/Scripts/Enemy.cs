0cf8cbcebf8c494ced629f27faf3b7036d9d09eb
James Hwang <james.hwang@case.edu>
2020-04-09 15:57:30 -0400

    merge two projects together

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    public float health = 1f;
+
+    public int moneyValue = 1;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+
+        if(wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            GameObject.FindObjectOfType<ScoreManager>().loseLife();
+            Destroy(gameObject);
+            return;
+        }
+        
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        GameObject.FindObjectOfType<ScoreManager>().money += moneyValue;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float roundTime = 5f;
+    private float countdown = 2f;
+
+    public Text WaveCountdownText;
+
+    private int waveIndex = 0;
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = roundTime;
+        }
+
+        countdown -= Time.deltaTime;
+
+        WaveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        // Debug.Log("Wave Incomming!");
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+         
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        GameObject.FindObjectOfType<ScoreManager>().wave += 1;
+
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
